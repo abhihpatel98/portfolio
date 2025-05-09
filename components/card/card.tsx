e7e52d862490318286fbad6c78849f48267ac1c8
Abhi Patelm <abhipatel181998@gmail.com>
2025-05-08 22:12:21 -0400

    Fix build issues.

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -20,15 +20,21 @@ const Card: React.FC<CardProps> = ({
     >
       {image && (
         <div className="card-image-container relative w-full pt-[56.25%]">
-          <img 
-            className="image absolute top-0 left-0 w-full h-full object-cover" 
-            src={image.url} 
-            alt={image.alt} 
+          <img
+            className="image absolute top-0 left-0 w-full h-full object-cover"
+            src={image.url}
+            alt={image.alt}
           />
         </div>
       )}
-      {/* <div className="section-one px-3 py-2 bg-opacity-80">{sectionOneContent}</div> */}
-      <div className="section-two flex-grow flex flex-col px-3 py-3">{sectionTwoContent}</div>
+      {false && (
+        <div className="section-one px-3 py-2 bg-opacity-80">
+          {sectionOneContent}
+        </div>
+      )}
+      <div className="section-two flex-grow flex flex-col px-3 py-3">
+        {sectionTwoContent}
+      </div>
     </div>
   );
 };
